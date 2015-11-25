78a8b254b843c826d93e45151e28dea7c5a4ab48
luzzyzhang <luzzyzhang@gmail.com>
2015-11-25 12:15:18 +0800

    add new dcostrng

--- data_structures_and_algorithms/remove_dupulicates_keep_order.py
+++ data_structures_and_algorithms/remove_dupulicates_keep_order.py
@@ -1,4 +1,13 @@
-# This only work if the items in the sequence are harshble.
+#################################################################
+# Use generator funcitons(yield) to be extremely general purpose
+# not necessarily tied directly to list processing.
+# yield will return iterable object
+# For example if read a file, eliminating duplicate lines
+# wiht open(somefile, 'r') as f:
+#     for line in dedupe(f):
+#         ...
+##################################################################
+# Only work if the items in the sequence are harshble.
 lst = [2, 1, 3, 1, 1, 2, 5, 9, 9, 10, 5]
 
 
@@ -13,6 +22,7 @@ print nlst
 
 
 # If sequence of unhashable types (such as dicts)
+# Both hashble and unhashble work well
 l_dct = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3},
          {'x': 1, 'y': 2}, {'x': 2, 'y': '4'}]
 
