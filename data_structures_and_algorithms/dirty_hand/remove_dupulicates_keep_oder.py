b9cdb9c8ef5ea4a46a1b444d40694ba985f6c6eb
luzzyzhang <luzzyzhang@gmail.com>
2015-11-26 22:43:16 +0800

    Add dirty_hand to practice

---
+++ data_structures_and_algorithms/dirty_hand/remove_dupulicates_keep_oder.py
@@ -0,0 +1,31 @@
+def dedupe(items):
+    seen = set()
+    for item in items:
+        if item not in seen:
+            yield item
+            seen.add(item)
+
+lsts = [1, 2, 3, 3, 2, 5, 3, 4, 4]
+nlst = list(dedupe(lsts))
+print nlst
+
+
+dcts = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3},
+         {'x': 1, 'y': 2}, {'x': 2, 'y': '4'}]
+
+def dedupe_unhash(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+ndcts = list(dedupe_unhash(dcts, key=lambda d: (d['x'], d['y'])))
+print ndcts
+ndcts2 = list(dedupe_unhash(dcts, key=lambda dct: dct['y']))
+print ndcts2
+ndcts3 = list(dedupe_unhash(dcts, key=lambda d: d['x']))
+print ndcts3
+
+
