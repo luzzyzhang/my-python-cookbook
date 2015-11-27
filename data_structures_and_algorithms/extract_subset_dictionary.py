ac9a7c7ef81e8c69210d584c28c2a2e5c6a2fc34
luzzyzhang <luzzyzhang@gmail.com>
2015-11-27 22:33:11 +0800

    Add item in origin dictionary

--- data_structures_and_algorithms/extract_subset_dictionary.py
+++ data_structures_and_algorithms/extract_subset_dictionary.py
@@ -1,11 +1,6 @@
 # Extracting a Subset of a Dictionary
-prices = {
-    'ACME': 45.25,
-    'AAPL': 612.78,
-    'IBM': 205.55,
-    'HPQ': 37.20,
-    'FB': 10.75
-}
+prices = {'ACME': 45.25, 'AAPL': 612.78, 'GOOG': 10,
+          'IBM': 205.55, 'HPQ': 37.20, 'FB': 10.75}
 
 # Make new dictionary of all prices over 200
 new_prices1 = {key: value for key, value in prices.items() if value > 200}
