c024bc5ecaf1ea8dde2f561d16c076c019e37d13
luzzyzhang <luzzyzhang@gmail.com>
2015-11-27 22:52:23 +0800

    Add anthor way to achieve

--- data_structures_and_algorithms/extract_subset_dictionary.py
+++ data_structures_and_algorithms/extract_subset_dictionary.py
@@ -6,6 +6,13 @@ prices = {'ACME': 45.25, 'AAPL': 612.78, 'GOOG': 10,
 new_prices1 = {key: value for key, value in prices.items() if value > 200}
 print new_prices1
 # Make a new dictionary of tech company name
-tech_names = ['AAPL', 'IBM', 'HPQ', 'MSFT']
+tech_names = {'AAPL', 'IBM', 'HPQ', 'MSFT'}
 new_prices = {key: value for key, value in prices.items() if key in tech_names}
 print new_prices
+
+
+# Another way to extrac a subset dictionary, but speed slower
+n_prices1 = dict((key, value) for key, value in prices.items() if value > 200)
+print n_prices1
+n_prices2 = {key: prices[key] for key in prices.keys() if key in tech_names}
+print n_prices2
