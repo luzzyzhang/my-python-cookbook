eef4706fd7561c093e77f46d930aafabcb5d7e21
luzzyzhang <luzzyzhang@gmail.com>
2015-11-30 22:20:14 +0800

    Populate named tuples have optional or missing fields

--- data_structures_and_algorithms/map_names_into_sequence_elements.py
+++ data_structures_and_algorithms/map_names_into_sequence_elements.py
@@ -32,3 +32,24 @@ def compute_total(records):
         s = Stock(*rec)
         total += s.shares * s.price
     return total
+s = Stock('AAAA', 10010, 11.11)
+print s
+# s.shares = 75  # nameetuple is immutable
+s = s._replace(shares=75)
+print s
+
+
+# _replace() method can be populate named tuples that have optional or
+# missing fields
+Stock = namedtuple('Stock', ['name', 'shares', 'price', 'date', 'time'])
+# Create a prototype instance
+stock_prototype = Stock('', 0, 0.0, None, None)
+# Function to convert a dictionary to a Stock
+
+
+def dict_to_stock(s):
+    return stock_prototype._replace(**s)
+dct_a = {'name': 'PPOT', 'shares': 100, 'price': 123.45}
+print dict_to_stock(dct_a)
+dct_b = {'name': 'MMM', 'shares': 22, 'price': 222.2, 'date': '12/17/2015'}
+print dict_to_stock(dct_b)
