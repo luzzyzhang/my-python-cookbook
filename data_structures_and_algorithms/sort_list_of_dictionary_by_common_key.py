80cbb00f202b97c1fa9b73cd4f94c995c1ed66ee
luzzyzhang <luzzyzhang@gmail.com>
2015-12-05 00:05:37 +0800

    Use lambda expressiong

--- data_structures_and_algorithms/sort_list_of_dictionary_by_common_key.py
+++ data_structures_and_algorithms/sort_list_of_dictionary_by_common_key.py
@@ -7,11 +7,17 @@ rows = [
     {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
     {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
 ]
-rows_by_fname = sorted(rows, key=itemgetter('fname'))
+# use itemgetter() a bit faster run
+# rows_by_fname = sorted(rows, key=itemgetter('fname'))
+rows_by_fname = sorted(rows, key=lambda r: r['fname'])
 rows_by_uid = sorted(rows, key=itemgetter('uid'))
-rows_by_lfname = sorted(rows, key=itemgetter('lname', 'fname'))
+min_row_uid = min(rows, key=itemgetter('uid'))
+# rows_by_lfname = sorted(rows, key=itemgetter('lname', 'fname'))
+rows_by_lfname = sorted(rows, key=lambda r: (r['lname'], r['fname']))
 print rows_by_fname
 print 50*'~'
 print rows_by_uid
 print 50*'~'
 print rows_by_lfname
+print 50*'~'
+print min_row_uid
