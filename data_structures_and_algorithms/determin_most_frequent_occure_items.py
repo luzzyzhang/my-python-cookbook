68bc2f5a327d6cdee5f018c452aeed417d603af3
lz <ekitenso@ksxing.local>
2015-12-05 23:13:49 +0800

    Determine the most frequently occuring items in a sequence

---
+++ data_structures_and_algorithms/determin_most_frequent_occure_items.py
@@ -0,0 +1,7 @@
+# Determin the most frequecntly occurring of items in sequence
+words = [
+    'look', 'into', 'my', 'eyes', 'look', 'into', 'my', 'eyes',
+    'the', 'eyes', 'the', 'eyes', 'the', 'eyes', 'not', 'around',
+    'the', 'eyes', "don't", 'look', 'arround', 'the', 'eyes',
+    'look', 'into', 'my', 'eyes', "you're", 'under'
+]
