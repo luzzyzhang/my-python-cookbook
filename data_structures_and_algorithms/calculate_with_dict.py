a1b4d68c32e866e2a1f8a9c60926b355c36f7fa0
luzzyzhang <luzzyzhang@gmail.com>
2015-12-08 23:19:51 +0800

    Calculate with dictionaries

---
+++ data_structures_and_algorithms/calculate_with_dict.py
@@ -0,0 +1,16 @@
+# Perform calculations on dictionary data
+prices = {
+    'ACME': 45.23,
+    'AAPL': 612.78,
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
+}
+
+min_price = min(zip(prices.values(), prices.keys()))
+max_price = max(zip(prices.values(), prices.keys()))
+prices_sorted = sorted(zip(prices.values(), prices.keys()))
+print min_price
+print max_price
+print zip(prices.values(), prices.keys())
+print prices_sorted
