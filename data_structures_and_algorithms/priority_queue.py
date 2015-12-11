cba7e4a7dc776da80a85c05e12a04aff1ae96249
luzzyzhang <luzzyzhang@gmail.com>
2015-12-12 00:05:25 +0800

    Test priority queue

--- data_structures_and_algorithms/priority_queue.py
+++ data_structures_and_algorithms/priority_queue.py
@@ -13,3 +13,22 @@ class PriorityQueue:
 
     def pop(self):
         return heapq.heappop(self._queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+
+q = PriorityQueue()
+q.push(Item('foo'), 1)
+q.push(Item('bar'), 5)
+q.push(Item('spam'), 4)
+q.push(Item('grok'), 1)
+print q.pop()
+print q.pop()
+print q.pop()
+print q.pop()
