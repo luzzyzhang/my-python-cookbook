31720198b7eabb8e80e1cd7ae3c0d64fc163e199
luzzyzhang <luzzyzhang@gmail.com>
2015-12-15 22:53:54 +0800

    Keep the laste N items

---
+++ data_structures_and_algorithms/keep_last_n_items.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+
+def search(lines, pattern, history=5):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
