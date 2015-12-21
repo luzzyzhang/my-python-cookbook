301ad369a203567664cba5627ac3cc726ea50629
luzzyzhang <luzzyzhang@gmail.com>
2015-12-21 21:38:03 +0800

    Fix typo

--- strings_text/split_strings_multiple_delimiters.py
+++ strings_text/split_strings_multiple_delimiters.py
@@ -2,7 +2,7 @@
 # delimiters or account of possible whitespaces around delimiters
 import re
 line = 'asdf fjdk; afed, fjek,asdf,         foo'
-new_line = re.split(r'[:,\s]\s*', line)
+new_line = re.split(r'[;,\s]\s*', line)
 print new_line
 fields = re.split(r'(;|,|\s)\s*', line)
 print fields
