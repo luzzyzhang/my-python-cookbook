5aff613a89aa6da885bd439d6ce5b9bc9c63b37d
luzzyzhang <luzzyzhang@gmail.com>
2015-12-21 21:36:44 +0800

    nothing

--- strings_text/split_strings_multiple_delimiters.py
+++ strings_text/split_strings_multiple_delimiters.py
@@ -4,3 +4,14 @@ import re
 line = 'asdf fjdk; afed, fjek,asdf,         foo'
 new_line = re.split(r'[:,\s]\s*', line)
 print new_line
+fields = re.split(r'(;|,|\s)\s*', line)
+print fields
+values = fields[::2]
+delimiters = fields[1::2] + ['']
+print values
+print delimiters
+# Reform the line using the same delimiters
+s = ''.join(v+d for v, d in zip(values, delimiters))
+print s
+match = re.split(r'(?:,|;|\s)\s*', line)
+print match
