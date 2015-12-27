6b3333ed8354444048c57bc406be96d613ae6f07
luzzyzhang <luzzyzhang@gmail.com>
2015-12-27 20:19:07 +0800

    nothing

--- strings_text/search_replace_text.py
+++ strings_text/search_replace_text.py
@@ -4,7 +4,27 @@ text = 'zhanglu say zhaolingpu ni hai hao ma?'
 n_text = text.replace('say', '@')
 print n_text
 # for more complicated pattern use sub() funtion/mothod in re module
-# replace 20/11/2011 tot 2011-11-20
+# replace 11/20/2011 tot 2011-11-20
 import re
-date = 'Today Merry Christams 25/12/2015, I remenber you 09/08/2007'
-re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', date)
+date = 'Today Merry Christams 12/25/2015, I remenber you 11/25/2007'
+new_date = re.sub(r'(\d+)/(\d+)/(\d+)', r'\3-\1-\2', date)
+print new_date
+# perform repeat substitutions
+datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
+date2 = datepat.sub(r'\3-\1-\2', date)
+print date2
+
+# More complicated substitutions to specify a substitution callback function
+from calendar import month_abbr
+
+
+def change_date(m):
+    mon_name = month_abbr[int(m.group(1))]
+    return '{} {} {}'.format(m.group(2), mon_name, m.group(3))
+date3 = datepat.sub(change_date, date)
+print date3
+
+# Use re.subn() to show how many substitutions were replaced
+newdate, n = datepat.subn(r'\3-\1-\2', date)
+print newdate
+print n
