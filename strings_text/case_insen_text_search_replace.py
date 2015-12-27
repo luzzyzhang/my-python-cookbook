46e85934a2aa6c2c329c450ed781d20c12ee909f
luzzyzhang <luzzyzhang@gmail.com>
2015-12-27 21:31:17 +0800

    nothing

--- strings_text/case_insen_text_search_replace.py
+++ strings_text/case_insen_text_search_replace.py
@@ -1,7 +1,24 @@
 # Search replace text in a case-insensitive manner
 import re
-text = 'UPPER PYTHON, lower python, MIxed Python'
-find_py  = re.findall('python', text, flags=re.IGNORECASE)
+text = 'UPPER PYTHON, lower python, Mixed Python'
+find_py = re.findall('python', text, flags=re.IGNORECASE)
 print find_py
 new_text = re.sub('python', 'Golang', text, flags=re.IGNORECASE)
 print new_text
+
+
+def matchcase(word):
+    def replace(m):
+        text = m.group()
+        if text.isupper():
+            return word.upper()
+        elif text.islower():
+            return word.lower()
+        elif text[0].isupper():
+            return word.capitalize()
+        else:
+            return word
+
+texts = 'UPPER PYTHON, lower python, Mixed Python'
+case_replace = re.sub('python', matchcase('snake'), texts, flags=re.IGNORECASE)
+print case_replace
