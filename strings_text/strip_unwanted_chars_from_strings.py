b63c72737cd659c90d02dcc33ac9026e4267b335
luzzyzhang <luzzyzhang@gmail.com>
2016-01-04 22:37:21 +0800

    Strip inner space

---
+++ strings_text/strip_unwanted_chars_from_strings.py
@@ -0,0 +1,21 @@
+# strip() method used to strip characters from the beginning or end of a
+# string. lstrip() and rstrip() perform stripping from the left or right
+# Whitespace stripping
+s = '     hello world   \n'
+print s.strip()
+print s.lstrip()
+print s.rstrip()
+# Characters stripping
+t = '---------hello=========='
+print t.lstrip('-')
+print t.rstrip('=')
+print t.strip('-=')
+# stripping does not apply to any text in the middle of a string
+s = '    hello     world    \n'
+print s.strip()
+# For inner space use replace() method or regular expression substitution
+print 50*'~'
+s = 'hello     world    \n'
+print s.replace(' ', '')
+import re
+print re.sub('\s+', ' ', s)
