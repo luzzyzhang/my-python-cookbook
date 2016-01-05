a77c05203f8672154897af51e18770d2a02fdc3b
luzzyzhang <luzzyzhang@gmail.com>
2016-01-05 21:17:33 +0800

    Poslish

--- strings_text/sanitize_cleanup_text.py
+++ strings_text/sanitize_cleanup_text.py
@@ -1,13 +1,16 @@
 s = 'p\xfdt\u0125\xf6\xf1\x0cis\tawesome\r\n'
 print(s)
+# Clean up whitespace
 remap = {ord('\t'): ' ', ord('\f'): ' ', ord('\r'): None}
 a = s.translate(remap)
 print(a)
-# Clean up whitespace, strip diacritical marks
+# Strip diacritical marks
 import unicodedata
 import sys
+# Dictionary mapping every Unicode combing character to None
 cmb_chrs = dict.fromkeys(c for c in range(sys.maxunicode)
                          if unicodedata.combining(chr(c)))
+# print(cmb_chrs)
 b = unicodedata.normalize('NFD', a)
 print(b)
 print(b.translate(cmb_chrs))
