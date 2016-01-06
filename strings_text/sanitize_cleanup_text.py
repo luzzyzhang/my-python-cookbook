3a64d4294337d0e06d3917879535a8a594d36d08
luzzyzhang <luzzyzhang@gmail.com>
2016-01-06 22:07:22 +0800

    Add new example

--- strings_text/sanitize_cleanup_text.py
+++ strings_text/sanitize_cleanup_text.py
@@ -14,3 +14,11 @@ cmb_chrs = dict.fromkeys(c for c in range(sys.maxunicode)
 b = unicodedata.normalize('NFD', a)
 print(b)
 print(b.translate(cmb_chrs))
+# Translate table maps all Unicode decimal digit characters to equal ASCII
+digitmap = {c: ord('0') + unicodedata.digit(chr(c))
+            for c in range(sys.maxunicode)
+            if unicodedata.category(chr(c)) == 'Nd'}
+print(len(digitmap))
+# Arabic digits
+x = '\u0661\u0662\u0663'
+print(x.translate(digitmap))
