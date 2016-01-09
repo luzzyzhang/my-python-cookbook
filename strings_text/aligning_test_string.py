1d384bd40fb5d9f9f921a76071776610b0414fb0
luzzyzhang <luzzyzhang@gmail.com>
2016-01-09 23:35:58 +0800

    Add exampes

--- strings_text/aligning_test_string.py
+++ strings_text/aligning_test_string.py
@@ -9,6 +9,16 @@ print text.center(20, '*')
 ali_text1 = format(text, '>20')  # left
 ali_text2 = format(text, '<20')  # right
 ali_text3 = format(text, '^20')  # center
+ali_text4 = format(text, '=>20s')  # fill character other than space
+ali_text5 = format(text, '*^20s')
 print ali_text1
 print ali_text2
 print ali_text3
+print ali_text4
+print ali_text5
+# format multiple values
+words = '{:>10s} {:<10s} {:^10s}'.format('Happy', 'Hack', 'Day')
+print words
+x = 1.2345
+print format(x, '>10')
+print format(x, '^10.2f')
