af3f68b5a26c05a23fe628f98bbae5d27d716ca4
luzzyzhang <luzzyzhang@gmail.com>
2016-01-09 23:21:40 +0800

    Add example

--- strings_text/aligning_test_string.py
+++ strings_text/aligning_test_string.py
@@ -1,1 +1,14 @@
 # Format text with some of alignment applied
+text = "WelcomeToTop"
+print text.ljust(20)
+print text.rjust(20)
+print text.center(20)
+print text.rjust(20, '=')
+print text.center(20, '*')
+# Use format() function to easily align things >^<
+ali_text1 = format(text, '>20')  # left
+ali_text2 = format(text, '<20')  # right
+ali_text3 = format(text, '^20')  # center
+print ali_text1
+print ali_text2
+print ali_text3
