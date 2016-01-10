22d080dd4027ff5e5412d52244f927d2a701cf9b
luzzyzhang <luzzyzhang@gmail.com>
2016-01-10 22:02:13 +0800

    Add new examples

--- strings_text/combine_concatenate_strings.py
+++ strings_text/combine_concatenate_strings.py
@@ -23,3 +23,55 @@ data = ['ACME', 10, 199.1]
 # print ','.join(data)  # error
 data_str = ','.join(str(d) for d in data)  # convert int or float to sting
 print data_str
+# print(a + ':' + b + ':' + c)  # ugly
+# print(':'.join([a, b, c]))  # still ugly
+# print(a, b, c, seq=':')  # better
+# Mixing I/O operations and string concatenation
+# Version 1 (string concatenation)
+# If two strings are small better performance tan version 2
+'''
+f.write(chunk1 + chunk2)
+'''
+# Version 2 (separate I/O operations)
+# If two strings are large better performance than version 1
+'''
+f.write(chunk1)
+f.write(chunk2)
+'''
+print 50*'-'
+
+
+def sample():
+    yield 'Is'
+    yield 'Chicago'
+    yield 'Not'
+    yield 'Chicageo'
+text = ','.join(sample())
+print text
+with open('test.txt', 'w') as f:
+    for part in sample():
+        print part
+        f.write(part)
+
+
+print 50*'*'
+# Hybrid scheme that's smart about combining I/O operations
+def combine(source, maxsize):
+    parts = []
+    size = 0
+    for part in source:
+        parts.append(part)
+        size += len(part)
+        print size
+        if size > maxsize:
+            yield '$'.join(parts)
+            parts = []
+            size = 0
+    yield '-'.join(parts)
+
+with open('test2.txt', 'w') as f:
+    for part in combine(sample(), 32768):
+        print 50*'='
+        print part
+        print 50*'='
+        f.write(part)
