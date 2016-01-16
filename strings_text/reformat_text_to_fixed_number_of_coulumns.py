5208b7dfaa253d6107d9f15b090c496fc973e19c
luzzyzhang <luzzyzhang@gmail.com>
2016-01-16 22:44:22 +0800

    Reformating text to fixed number of columns

--- strings_text/reformat_text_to_fixed_number_of_coulumns.py
+++ strings_text/reformat_text_to_fixed_number_of_coulumns.py
@@ -1,1 +1,13 @@
 # Long strings to reformat specified number of columns
+s = "Look into my eyes, look into my eye, the eyes, the eyes, \
+the eyes, not around the eyes, don't look around the eyes, \
+look into my eyes, you're under."
+import textwrap
+print textwrap.fill(s, 50)
+print 50*'*'
+print textwrap.fill(s, 40, initial_indent='    ')
+print 50*'~'
+print textwrap.fill(s, 40, subsequent_indent='    ')
+# Only python3
+import os
+print os.get_terminal_size().columns
