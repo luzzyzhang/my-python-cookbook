4fb3f88f9c24851d8ce2bf8bfed1792293ea386b
luzzyzhang <luzzyzhang@gmail.com>
2016-01-17 21:42:16 +0800

    Add examples

--- strings_text/handle_html_xml_entities_in_text.py
+++ strings_text/handle_html_xml_entities_in_text.py
@@ -4,3 +4,15 @@ print(s)
 print(html.escape(s))
 # Disable escaping of quotes
 print(html.escape(s, quote=False))
+print(50*'*')
+t = 'Spicy Jalapeño'
+nt = t.encode('ascii', errors='xmlcharrefreplace')
+print(nt)
+print(50*'~')
+s = 'Spicy &quot;Jalape&#241;o&quote.'
+from html.parser import HTMLParser
+p = HTMLParser()
+print(p.unescape(s))
+t = 'The prompt is &gt;&gt;&gt;'
+from xml.sax.saxutils import unescape
+print(unescape(t))
