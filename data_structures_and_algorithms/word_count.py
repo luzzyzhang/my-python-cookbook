052b96bba1dff3e8b7d583a489b5eb92792d2fab
luzzy <luzzyzhang@gmail.com>
2016-12-09 15:49:34 +0800

    Input a txt file give word-count dict

---
+++ data_structures_and_algorithms/word_count.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import sys
+from collections import Counter
+
+
+def word_count(filename):
+    wc = {}
+    # Solution one
+    with open(filename, 'r') as f:
+        words = f.read().lower().split()
+    # for word in words:
+    #     if word not in wc:
+    #         wc[word] = 1
+    #     else:
+    #         wc[word] += 1
+    wc = Counter(words)
+    # Solution two
+    # with open(filename, 'r') as f:
+    #     for line in f:
+    #         words = line.lower().split()
+    #         for word in words:
+    #             if word not in words:
+    #                 wc[word] = 1
+    #             else:
+    #                 wc[word] += 1
+
+    return wc
+
+
+def print_words(filename):
+    wc = word_count(filename)
+    for word, count in sorted(zip(wc.keys(), wc.values())):
+        print word, count
+
+
+def print_top(filename, n=20):
+    wc = word_count(filename)
+    for count, word in sorted(zip(wc.values(), wc.keys()), reverse=True)[0:n]:
+        print word, count
+    # Other method
+    for word, count in wc.most_common(3):
+        print word, count
+
+
+def main():
+    if len(sys.argv) != 3:
+        print 'usage: ./wordcount.py {--count | --topcount} file'
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print 'Unknow option: ' + option
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
