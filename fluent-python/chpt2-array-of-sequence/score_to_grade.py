00a7a5a8b9f4ac29a31d5dea71e8eb5e359b9f9b
luzzy <luzzyzhang@gmail.com>
2017-02-28 22:01:35 +0800

    Given a test score, grade returns the corresponding letter grade.

---
+++ fluent-python/chpt2-array-of-sequence/score_to_grade.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import bisect
+
+
+def grade(score, breakpoints=[60, 70, 80, 90], grades='FDCBA'):
+    i = bisect.bisect(breakpoints, score)
+    return grades[i]
+
+
+if __name__ == '__main__':
+    print [grade(score) for score in [33, 99, 77, 70, 89, 90, 100]]
