f951929aace3095d2315ae91ce7fd13f61699393
luzzy <luzzyzhang@gmail.com>
2017-02-28 22:06:52 +0800

    Polish

--- fluent-python/chpt2-array-of-sequence/score_to_grade.py
+++ fluent-python/chpt2-array-of-sequence/score_to_grade.py
@@ -8,4 +8,4 @@ def grade(score, breakpoints=[60, 70, 80, 90], grades='FDCBA'):
 
 
 if __name__ == '__main__':
-    print [grade(score) for score in [33, 99, 77, 70, 89, 90, 100]]
+    print([grade(score) for score in [33, 99, 77, 70, 89, 90, 100]])
