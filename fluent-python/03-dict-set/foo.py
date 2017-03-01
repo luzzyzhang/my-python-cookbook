82c62ca5a72774028134b8cfaa0d161fafdd8ea5
luzzy <luzzyzhang@gmail.com>
2017-03-01 22:10:41 +0800

    Reorgnize code

---
+++ fluent-python/03-dict-set/foo.py
@@ -0,0 +1,3 @@
+import sys
+sys.stdout = open('zen.txt', 'w')
+import this
---
+++ fluent-python/03-dict-set/index0.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Build an index mapping word -> list of occurrences"""
+
+import sys
+import re
+
+
+WORD_RE = re.compile(r'\w+')
+
+
+index = {}
+
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            # this is ugly; coded like this to make a point
+            occurences = index.get(word, [])
+            occurences.append(location)
+            index[word] = occurences
+
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
