65f4731e16f492b166f27b3c106647e5e2842a24
luzzy <luzzyzhang@gmail.com>
2017-03-05 19:39:52 +0800

    Default encoding

---
+++ fluent-python/04-text-byte/default_encodings.py
@@ -0,0 +1,20 @@
+import sys
+import locale
+
+expressions = """
+        locale.getpreferredencoding()
+        type(my_file)
+        my_file.encoding
+        sys.stdout.isatty()
+        sys.stdout.encoding
+        sys.stdin.isatty()
+        sys.stdin.encoding
+        sys.stderr.isatty()
+        sys.stderr.encoding
+        sys.getdefaultencoding()
+        sys.getfilesystemencoding()
+    """
+my_file = open('dummy', 'w')
+for expression in expressions.split():
+    value = eval(expression)
+    print(expression.rjust(30), '->', repr(value))
