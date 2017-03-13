87b78eeb4beab0b6ee08c953bc07285b229e6780
luzzy <luzzyzhang@gmail.com>
2017-03-13 23:02:18 +0800

    Test

--- data_structures_and_algorithms/LRUCache.py
+++ data_structures_and_algorithms/LRUCache.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 class LRUCache:
-
     def __init__(self, user_function, maxsize=1024):
         # Link layout:     [PREV, NEXT, KEY, RESULT]
         self.root = root = [None, None, None, None]
