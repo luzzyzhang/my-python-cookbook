a8a60fb38b2d75b6b0399f405be2b2b8ebe1e311
luzzy <luzzyzhang@gmail.com>
2017-04-01 22:00:38 +0800

    Vector2d fmt snippet

---
+++ fluent-python/09-pythonic-obj/vector2d_v2_fmt_snippet.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+
+from array import array
+import math
+
+
+class Vector2d(object):
+    typecode = 'd'
+
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{}({!r}, {!r})'.format(class_name, *self)
+
+    def __str__(self):
+        return str(tuple(self))
+
+    def __bytes__(self):
+        return bytes(array(Vector2d.typecode, self))
+
+    def __eq__(self, other):
+        return tuple(self) == tuple(other)
+
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def angle(self):
+        return math.atan2(self.y, self.x)
+
+    def __format__(self, fmt_spec=''):
+        if fmt_spec.endswith('p'):
+            fmt_spec = fmt_spec[:-1]
+            coords = (abs(self), self.angle())
+            outer_fmt = '<{}, {}>'
+        else:
+            coords = self
+            outer_fmt = '({}, {})'
+        components = (format(c, fmt_spec) for c in coords)
+        return outer_fmt.format(*components)
+
+    @classmethod
+    def frombytes(cls, octets):
+        memv = memoryview(octets).cast(cls.typecode)
+        return cls(*memv)
