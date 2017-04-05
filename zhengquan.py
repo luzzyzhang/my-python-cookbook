b8b6ee1e66eacd28ccb735ce4e7d864dd76052a2
luzzy <luzzyzhang@gmail.com>
2017-04-05 23:52:23 +0800

    zhenquan

--- zhengquan.py
+++ zhengquan.py
@@ -6,11 +6,25 @@ import requests
 
 my_ak = 'kdPVvSv7n0BZl70MQgDPiQ9U6WQfeYHp'
 playload = {'q': '证券营业部', 'region': '北京市', 'output': 'json',
-            'ak': my_ak, 'page_num': 2, 'page_size': 20}
+            'ak': my_ak, 'page_num': 0, 'page_size': 20}
 r = requests.get('http://api.map.baidu.com/place/v2/search', params=playload)
 data = r.json()
-pprint(data)
-print([(d['name'], d['address'], d.get('telephone')) for d in data['results']])
+page_nums = data['total'] // 20
+result = []
+for i in range(page_nums):
+    playload['page_num'] = i
+    r = requests.get('http://api.map.baidu.com/place/v2/search', params=playload)
+    r = r.json()['results']
+    result.append((d['name'], d.get('telephone'), d['address']) for d in r if d.get('telephone'))
+print(list(result))
+with open('r.txt', 'w') as f:
+    header = "{}, {:>10s}, {:>10s} \n".format('营业部名', '电话', '地址')
+    f.write(header)
+    for l in result:
+        for i in l:
+            info = "{}, {:>10s}, {:>10s} \n".format(*i)
+            f.write(info)
+# print([(d['name'], d['address'], d.get('telephone')) for d in data['results']])
 # data = json.load(open('data_out.json'))
 # print(len(data['content']))
 # print(data['content'][8]['name'])
