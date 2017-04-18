4cea5220447ca46714f24ccdb8ac29f4b591dade
luzzy <luzzyzhang@gmail.com>
2017-04-18 23:21:17 +0800

    Redis check 3 time

--- try_redis.py
+++ try_redis.py
@@ -4,6 +4,16 @@
 import redis
 
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
-r.set('foo', '456')
-print(r.get('foo'))
+# r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = redis.Redis(host='localhost', port=6379, db=0)
+# r.set('foo', '1')
+
+if not r.get('mytest'):
+    r.set('mytest', 1)
+    print(r.get('mytest'))
+elif int(r.get('mytest')) == 3:
+    print('Stop')
+    print(r.get('mytest'))
+else:
+    r.incr('mytest')
+    print(r.get('mytest'))
