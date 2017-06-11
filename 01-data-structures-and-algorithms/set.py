f3439049ee22be9f5259bab444310933f1d82ecd
luzzy <luzzyzhang@gmail.com>
2017-06-11 21:27:28 +0800

    Update data structure set

--- 01-data-structures-and-algorithms/set.py
+++ 01-data-structures-and-algorithms/set.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 
+from functools import reduce
+
 # set & | - ^
 a = {1, 2, 3}
 b = {2, 4, 5}
-print a & b, a | b, a - b, a ^ b
+print(a & b, a | b, a - b, a ^ b)
 l1 = [1, 2, 3]
-l2 = [2, 4, 5]
+l2 = [2, 4, 5, 3]
+l3 = [2, 3, 0, 1]
 s1, s2 = set(l1), set(l2)
-print s1 & s2, s1 | s2, s1 - s2, s1 ^ s2
+print(s1 & s2, s1 | s2, s1 - s2, s1 ^ s2)
+
+lst = [set(l1), set(l2), set(l3)]
+print(set.intersection(set(l1), set(l3)))
+print(set.intersection(*lst))
+print(reduce(lambda x, y: set(x) & set(y), lst))
