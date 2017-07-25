1767219eaf67a36d48bdfe92b10c0505860ed3d1
luzzy <luzzyzhang@gmail.com>
2017-07-25 23:30:07 +0800

    Add lazy version iterator

---
+++ fluent-python/14-it-generator/sentence_gen_lazy_version.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+import re
+import reprlib
+
+RE_WORD = re.compile('\w+')
+
+
+class Sentence(object):
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return 'Sentence({})'.format(reprlib.repr(self.text))
+
+    def __iter__(self):
+        for match in RE_WORD.finditer(self.text):
+            yield match.group()
