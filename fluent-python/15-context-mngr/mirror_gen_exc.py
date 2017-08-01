206fdce079c2e1595cc990281d82a39ed3657594
lz <zhanglu@moviewisdom.cn>
2017-08-01 10:32:02 +0800

    Add context manager demoe with exception

---
+++ fluent-python/15-context-mngr/mirror_gen_exc.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+import contextlib
+
+
+@contextlib.contextmanager
+def looking_glass():
+    import sys
+    original_write = sys.stdout.write
+
+    def reverse_write(text):
+        original_write(text[::-1])
+
+    sys.stdout.write = reverse_write
+    msg = ''
+    try:
+        yield 'JABBERWOCKY'
+    except ZeroDivisionError:
+        msg = 'Please DO NOT divide by zero!'
+    finally:
+        sys.stdout.write = original_write
+        if msg:
+            print(msg)
