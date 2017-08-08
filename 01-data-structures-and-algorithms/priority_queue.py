ad6b2e33165e47401badc161a1347fdc78d7f49f
lz <zhanglu@moviewisdom.cn>
2017-08-08 10:00:32 +0800

    Add comment to priority queue

--- 01-data-structures-and-algorithms/priority_queue.py
+++ 01-data-structures-and-algorithms/priority_queue.py
@@ -1,4 +1,5 @@
-# A queue sorts items by given priority
+# A queue sorts items by given priority (highest first)
+# The stand lib moudule queue.PriorityQueue (lowest first)
 import heapq
 
 
@@ -23,26 +24,27 @@ class Item:
         return 'Item({!r})'.format(self.name)
 
 
-q = PriorityQueue()
-q.push(Item('foo'), 1)
-q.push(Item('bar'), 5)
-q.push(Item('spam'), 4)
-q.push(Item('grok'), 1)
-print q.pop()
-print q.pop()
-print q.pop()
-print q.pop()
-# a = Item('foo')
-# b = Item('bar')
-# a < b
-# (priority, item) tuples can be compared priority different
-a = (1, Item('foo'))
-b = (5, Item('bar'))
-print a < b
-# c = (1, Item('grok'))
-# a < c
-a = (1, 0, Item('foo'))
-b = (5, 1, Item('bar'))
-c = (1, 2, Item('grok'))
-print a < b
-print a < c
+if __name__ == '__main__':
+    q = PriorityQueue()
+    q.push(Item('foo'), 1)
+    q.push(Item('bar'), 5)
+    q.push(Item('spam'), 4)
+    q.push(Item('grok'), 1)
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    print(q.pop())
+    # a = Item('foo')
+    # b = Item('bar')
+    # a < b
+    # (priority, item) tuples can be compared priority different
+    a = (1, Item('foo'))
+    b = (5, Item('bar'))
+    print(a < b)
+    # c = (1, Item('grok'))
+    # a < c
+    a = (1, 0, Item('foo'))
+    b = (5, 1, Item('bar'))
+    c = (1, 2, Item('grok'))
+    print(a < b)
+    print(a < c)
