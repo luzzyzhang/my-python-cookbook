fba44022a669c910146438a1755f738c71edb44c
luzzy <luzzyzhang@gmail.com>
2017-08-13 22:39:19 +0800

    Copy some taxi simulator doctest

--- fluent-python/16-coroutine/taxi_sim.py
+++ fluent-python/16-coroutine/taxi_sim.py
@@ -1,4 +1,49 @@
 # -*- coding: utf-8 -*-
+"""
+Taxi simulator with delay on output
+===================================
+This is a variation of ``taxi_sim.py`` which adds a ``-d`` comand-line
+option. When given, that option adds a delay in the main loop, pausing
+the simulation for .5s for each "minute" of simulation time.
+Driving a taxi from the console::
+    >>> from taxi_sim import taxi_process
+    >>> taxi = taxi_process(ident=13, trips=2, start_time=0)
+    >>> next(taxi)
+    Event(time=0, proc=13, action='leave garage')
+    >>> taxi.send(_.time + 7)
+    Event(time=7, proc=13, action='pick up passenger')
+    >>> taxi.send(_.time + 23)
+    Event(time=30, proc=13, action='drop off passenger')
+    >>> taxi.send(_.time + 5)
+    Event(time=35, proc=13, action='pick up passenger')
+    >>> taxi.send(_.time + 48)
+    Event(time=83, proc=13, action='drop off passenger')
+    >>> taxi.send(_.time + 1)
+    Event(time=84, proc=13, action='going home')
+    >>> taxi.send(_.time + 10)
+    Traceback (most recent call last):
+      File "<stdin>", line 1, in <module>
+    StopIteration
+Sample run with two cars, random seed 10. This is a valid doctest::
+    >>> main(num_taxis=2, seed=10)
+    taxi: 0  Event(time=0, proc=0, action='leave garage')
+    taxi: 0  Event(time=5, proc=0, action='pick up passenger')
+    taxi: 1     Event(time=5, proc=1, action='leave garage')
+    taxi: 1     Event(time=10, proc=1, action='pick up passenger')
+    taxi: 1     Event(time=15, proc=1, action='drop off passenger')
+    taxi: 0  Event(time=17, proc=0, action='drop off passenger')
+    taxi: 1     Event(time=24, proc=1, action='pick up passenger')
+    taxi: 0  Event(time=26, proc=0, action='pick up passenger')
+    taxi: 0  Event(time=30, proc=0, action='drop off passenger')
+    taxi: 0  Event(time=34, proc=0, action='going home')
+    taxi: 1     Event(time=46, proc=1, action='drop off passenger')
+    taxi: 1     Event(time=48, proc=1, action='pick up passenger')
+    taxi: 1     Event(time=110, proc=1, action='drop off passenger')
+    taxi: 1     Event(time=139, proc=1, action='pick up passenger')
+    taxi: 1     Event(time=140, proc=1, action='drop off passenger')
+    taxi: 1     Event(time=150, proc=1, action='going home')
+    *** end of events ***
+"""
 
 import time
 import queue
